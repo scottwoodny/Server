d1f2298c261f685533a6b83d86c3495661ca8fee
scott wood <scottwoodny@gmail.com>
2017-04-05 14:46:39 -0400

    my first website

---
+++ server.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+# specify two things
+
+get "/" do
+
+"Hellow World"
+# this is what executes when some one request some code
+# after the slash is the request
+
+# and what do you want to do or get
+end
+
+get "/wyncode" do
+  "Hello World"
+end
+
+# when you make the change you must then restart the server shut down then restart
+  # localhost  it is a microframework
+
+  # control c up and enter (brings back old program)
